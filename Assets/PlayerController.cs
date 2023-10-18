29531d2900982b4b6ef0a2407d0c04a56a19d97b
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-10-18 09:10:35 +0200

    Added jump

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,6 +7,11 @@ public class PlayerController : MonoBehaviour
   [SerializeField]
   float speed = 5;
 
+  [SerializeField]
+  float jumpForce = 100;
+
+  bool mayJump = true;
+
   // Start is called before the first frame update
   void Start()
   {
@@ -21,5 +26,22 @@ public class PlayerController : MonoBehaviour
     Vector2 movementX = new Vector2(moveX, 0);
 
     transform.Translate(movementX * speed * Time.deltaTime);
+
+    if (Input.GetAxisRaw("Jump") > 0 && mayJump == true)
+    {
+      Rigidbody2D rb = GetComponent<Rigidbody2D>();
+      Vector2 jump = Vector2.up * jumpForce;
+
+      rb.AddForce(jump);
+
+      mayJump = false;
+    }
+
+    if (Input.GetAxisRaw("Jump") == 0)
+    {
+      mayJump = true;
+    }
+
+
   }
 }
