0cb4250b3170faa9cf754a4c1a71c4229eaf898c
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-10-18 08:33:06 +0200

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  [SerializeField]
+  float speed = 5;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float moveX = Input.GetAxisRaw("Horizontal");
+
+    Vector2 movementX = new Vector2(moveX, 0);
+
+    transform.Translate(movementX * speed * Time.deltaTime);
+  }
+}
